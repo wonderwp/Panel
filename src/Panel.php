7825a523a7e64a2fa54335a77b26faceb31828bb
Jeremy Desvaux <jeremy@jdmweb.com>
2024-11-28 23:40:47 +0100

    refacto(panel): update with new definitions

--- src/Panel.php
+++ src/Panel.php
@@ -3,95 +3,12 @@
 namespace WonderWp\Component\Panel;
 
 use WonderWp\Component\Form\Field\FieldInterface;
+use WonderWp\Component\Panel\PostFieldPanel\PostFieldPanel;
 
-class Panel implements PanelInterface
+/**
+ * @deprecated Use PostFieldPanel instead
+ */
+class Panel extends PostFieldPanel implements PanelInterface
 {
-    /** @var string */
-    protected $id;
-    /** @var string */
-    protected $title;
-    /** @var FieldInterface[] */
-    protected $fields = [];
-    /** @var array */
-    protected $postTypes = [];
-
-    /** @inheritdoc */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /** @inheritdoc */
-    public function setId($id)
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /** @inheritdoc */
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    /** @inheritdoc */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    /** @inheritdoc */
-    public function getFields()
-    {
-        return $this->fields;
-    }
-
-    /** @inheritdoc */
-    public function setFields(array $fields)
-    {
-        $this->fields = $fields;
-
-        return $this;
-    }
-
-    /** @inheritdoc */
-    public function getPostTypes()
-    {
-        return $this->postTypes;
-    }
-
-    /** @inheritdoc */
-    public function setPostTypes(array $postTypes)
-    {
-        $this->postTypes = $postTypes;
-
-        return $this;
-    }
-
-    public function formatToDb($value)
-    {
-        if (is_array($value) || is_object($value)) {
-            $value = serialize($value);
-        }
-        if (is_string($value)) {
-            $value = stripslashes($value);
-        }//Because request adds a /
-
-        return $value;
-    }
-
-    public function formatFromDb($value)
-    {
-        if ($value == 'on') {
-            $value = 1;
-        } elseif (is_serialized($value)) {
-            $value = unserialize($value);
-        }
-
-        return $value;
-    }
 
 }
--- src/PanelInterface.php
+++ src/PanelInterface.php
@@ -3,68 +3,12 @@
 namespace WonderWp\Component\Panel;
 
 use WonderWp\Component\Form\Field\FieldInterface;
+use WonderWp\Component\Panel\PostFieldPanel\PostFieldPanelInterface;
 
-interface PanelInterface
+/**
+ * @deprecated Use PostFieldPanelInterface instead
+ */
+interface PanelInterface extends PostFieldPanelInterface
 {
-    /**
-     * @return mixed
-     */
-    public function getId();
 
-    /**
-     * @param mixed $id
-     *
-     * @return static
-     */
-    public function setId($id);
-
-    /**
-     * @return string
-     */
-    public function getTitle();
-
-    /**
-     * @param string $title
-     *
-     * @return static
-     */
-    public function setTitle($title);
-
-    /**
-     * @return FieldInterface[]
-     */
-    public function getFields();
-
-    /**
-     * @param FieldInterface[] $fields
-     *
-     * @return static
-     */
-    public function setFields(array $fields);
-
-    /**
-     * @return array
-     */
-    public function getPostTypes();
-
-    /**
-     * @param array $postTypes
-     *
-     * @return static
-     */
-    public function setPostTypes(array $postTypes);
-
-    /**
-     * @param mixed $value
-     *
-     * @return mixed
-     */
-    public function formatToDb($value);
-
-    /**
-     * @param mixed $value
-     *
-     * @return mixed
-     */
-    public function formatFromDb($value);
 }
